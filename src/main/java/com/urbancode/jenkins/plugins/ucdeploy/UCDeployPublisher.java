943952493702036d5ebf12f211b23bbbc08471a6
AjeetGupta <ajeetgupta.lpu@gmail.com>
2021-08-31 15:52:04 +0530

    Update UCDeployPublisher.java

--- src/main/java/com/urbancode/jenkins/plugins/ucdeploy/UCDeployPublisher.java
+++ src/main/java/com/urbancode/jenkins/plugins/ucdeploy/UCDeployPublisher.java
@@ -476,10 +476,10 @@ public class UCDeployPublisher extends Builder implements SimpleBuildStep {
      */
     public UCDeploySite getSite() {
         UCDeploySite[] sites = GLOBALDESCRIPTOR.getSites();
-        listener.getLogger().println("[COMING FROM GET SITE- CHECKING SITE VALUE]");
+//         listener.getLogger().println("[COMING FROM GET SITE- CHECKING SITE VALUE]");
 //         listener.getLogger().println(sites);
         if (siteName == null && sites.length > 0) {
-            listener.getLogger().println("[COMING FROM IF CONDITION]");
+//           listener.getLogger().println("[COMING FROM IF CONDITION]");
             // default
             return sites[0];
         }
@@ -488,7 +488,7 @@ public class UCDeployPublisher extends Builder implements SimpleBuildStep {
 //             listener.getLogger().println(site.getDisplayName());
 //             listener.getLogger().println(siteName);
             if (site.getDisplayName().equals(siteName)) {
-                listener.getLogger().println("PASSED ALL IF CONDITION");
+//                 listener.getLogger().println("PASSED ALL IF CONDITION");
 //                 listener.getLogger().println(site);
                 return site;
                 
